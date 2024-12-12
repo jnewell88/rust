35b47867fdcfc615058b9f7ab2a8a5f8a0093420
jnewell-spok <jnewell@spideroak.com>
2024-12-12 11:09:01 -0600

    added file reader

---
+++ file_reader/src/main.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // The first argument is the path that was used to call the program.
+    println!("My path is {}.", args[0]);
+    let file = File::open(args[1].clone());
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                panic!("File not found: {}", error)
+            }
+            _ => {
+                panic!("Error opening file: {}", error)
+            }
+        },
+    };
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
